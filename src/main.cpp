c886396e884a6d30386af3ff1d0fd8e85fbf92ca
hnspn <bobmatt911@gmail.com>
2019-09-06 11:14:58 -0400

    testing glew bindings

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -18,6 +20,16 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		return -1;
+	}
+
+	unsigned int a;
+	glGenBuffers(1, &a);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
